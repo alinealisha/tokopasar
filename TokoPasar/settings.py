0a5a60c51c9d8ab5c1dd6818b6ed86dc7a405c8d
alinealisha <alinealisha11@gmail.com>
2024-09-09 19:51:13 +0700

    push ketiga

--- TokoPasar/settings.py
+++ TokoPasar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)1*b#k03oneb8m!h6_)y0zc!$bt+_=r7i=3fibmabxjk3w4m-p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alisha-aline-TokoPasar.pbp.cs.ui.ac.id"]
 
 
 # Application definition
