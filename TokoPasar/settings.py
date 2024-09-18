7f4fa25c0f8e37a7e3c8c4c29d03cae53925f48e
alinealisha <alinealisha11@gmail.com>
2024-09-18 10:47:53 +0700

    Tugas 3 ver 2

--- TokoPasar/settings.py
+++ TokoPasar/settings.py
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://alisha-aline-tokopasar.pbp.cs.ui.ac.id", "https://alisha-aline-tokopasar.pbp.cs.ui.ac.id"]
